47217c623964a0d4bad7ecbcf47d281ce736545a
Satria-WS <extencezone02@gmail.com>
2024-05-11 12:32:32 +0700

    making method

--- basic rest api/index.js
+++ basic rest api/index.js
@@ -1,6 +1,21 @@
 import express from 'express';
 
 const app = express();
+
+//pettern pembuatan app , pattern routing
+//app.method(path,handler);
+// app.use('/', (req,res,next) => {
+//   res.send('Hello world');
+// })
+
+app.get('/', (req, res) => {
+  res.send('Hello get method');
+})
+
+app.post('/', (req,res) => {
+  res.send('Hello post method');
+})
+
 const port = 4000
 app.listen(port, () => {
   console.log('Server succesfull on port ' + port)
