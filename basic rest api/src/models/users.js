13fadd10b187713f7942fb2b91f4408e9606a56d
Satria-WS <extencezone02@gmail.com>
2024-05-12 21:40:21 +0700

    add sql

--- basic rest api/src/models/users.js
+++ basic rest api/src/models/users.js
@@ -12,7 +12,20 @@ const userModel = {
         }
       });
     });
-  }
+  },
+  createUser: async () => {
+    return new Promise((resolve, reject) => {
+      const query = "INSERT INTO users (name , email , address) VALUES ('SATrila','zz@gmail.com','sawper')";
+      dbPool.execute(query, (error, results, fields) => {
+        if (error) {
+          reject(new Error(`Error fetching users: ${error.message}`));
+        } else {
+          resolve(results);
+        }
+      });
+    });
+  },
+
 };
 
 export default userModel;
